a01f280e68ba589cf6ebaf9175801256fd121c7c
vmwhoami <vmwhoami@gmail.com>
2020-12-29 17:04:23 +0200

    Second commit install awebpack add all the linters

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './path/to/my/entry/file.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  }
+};
