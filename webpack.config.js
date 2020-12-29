277f33f2a18342de8762cccef302a41f85266831
vmwhoami <vmwhoami@gmail.com>
2020-12-29 17:20:47 +0200

    Add the styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
 
 module.exports = {
-  entry: './path/to/my/entry/file.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
 };
